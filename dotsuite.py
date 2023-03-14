abc4cacd257ac8537f8e4699f69fdf16747107a2
Nuwah12 <ngb23@pitt.edu>
2023-03-14 16:53:56 -0400

    dotsuite

---
+++ dotsuite.py
@@ -0,0 +1,20 @@
+"""DotSuite - Analyze dots in conjunction with other architectural proteins
+With the feature matrices prodiced in makeFeatureMatrices.py, integratively analyze called loops
+"""
+
+import cooler
+import bioframe
+import pybedtools
+import pandas as pd
+import numpy as np
+import argparse
+
+def main():
+    # Set up arguments
+
+    # Read in dots, give dot ID, split into 2 anchors ('left' anchor and 'right' anchor)
+
+    # Overlay onto interaction matrix, keeping dot identity
+
+if __name__ == "__main__":
+    main()
