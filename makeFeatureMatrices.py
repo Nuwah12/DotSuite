3f57891dcf2b9982e04bf6f873ccd559a55df770
Nuwah12 <ngb23@pitt.edu>
2023-03-15 10:21:31 -0400

    add progress bars

--- makeFeatureMatrices.py
+++ makeFeatureMatrices.py
@@ -11,6 +11,7 @@ import pybedtools
 import argparse
 import logging
 import pandas as pd
+from progress.bar import *
 
 def make_bins(binsize, chromsizes):
     """Make bins of size binsize
@@ -29,6 +30,9 @@ def main():
     parser.add_argument("--prefix", "-p", type=str, help="Prefix for output file: {prefix}_{res}bp.matrix", default="featureMatrix")
     #parser.add_argument("--dots", "-d", type=str, help="Path to dots (chr1,start1,end1,chr2,start2,end2), whose Anchors will be present as features in the output matrix")
     args=parser.parse_args()
+
+    ### Progress bar
+    bar = IncrementalBar('Processing Features', max = len(args.features))
 
     ### Compatibility checks
     # Feature file array size == feature name array size
@@ -63,24 +67,28 @@ def main():
         feature_bedtool = pybedtools.BedTool.from_dataframe(feature_dict[feature])
         # intersect with binned genome
         binned_features = binned_genome.intersect(feature_bedtool, wao=True)
-        #binned_features.to_dataframe(disable_auto_names=True, header=None).to_csv('overlap_test.tsv', sep="\t")
+        # binned_features.to_dataframe(disable_auto_names=True, header=None).to_csv('overlap_test.tsv', sep="\t")
         ### Make column for this feature
         # dict structure: {bin_id : feature_status (1/0)}
         binned_features = binned_features.to_dataframe(disable_auto_names=True, header=None)
+        # Loop through entire overlapped dataframe, if overlap size == 0 that bin does not have this feature, else it does 
+        # Progress bar
+        fulllen_bar = ChargingBar('Binning feature {}'.format(feature), max = len(binned_features.index))
         for i in range(0, len(binned_features.index)):
             if binned_features.iloc[i,7] != 0:
                 binned_features_dict[binned_features.iloc[i,3]] = 1
             else:
                 binned_features_dict[binned_features.iloc[i,3]] = 0
+            fulllen_bar.next()
         binned_features_master[feature] = binned_features_dict
-
-    #print(pd.DataFrame.from_dict(binned_features_master))
+        fulllen_bar.finish()
+        bar.next()
+        
+    bar.finish()
 
     final = pd.DataFrame.from_dict(binned_features_master)
-
     final.to_csv('{}_{}bp.matrix'.format(args.prefix, args.resolution), sep = "\t")
-    #print(len(binned_features_master))
-            
+    
 if __name__ == "__main__":
     main()
 
